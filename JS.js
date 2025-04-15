d294ac2f3ad949c91c2c9b6ec0aa8a2094fa95e7
Yohaan-TheTechGod <yohaandosaj123@gmail.com>
2025-04-15 16:49:02 +1000

    Fixed Syntax Errors

--- JS.js
+++ JS.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', () => {
     const scrollText = document.getElementById('scroll-text');
     if (
       window.innerHeight + window.scrollY >=
-      document..body.offsetHeight - 50
+      document.body.offsetHeight - 50
     ) {
       scrollText.classList.add('visible');
     } else {
